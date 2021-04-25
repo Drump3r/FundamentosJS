2854af7878ee5769e8d2f33181da4c370f728283
Brian <jacklevybrsg@gmail.com>
2021-04-25 09:42:48 -0500

    Condicionales

---
+++ clase10.js
@@ -0,0 +1,19 @@
+var persona = {
+    nombre: 'Sacha',
+    apellido: 'Lifszyc',
+    edad: 28,
+    ingeniero: true,
+    cocinero: false,
+    cantante: false,
+    dj: false,
+    guitarrista: false,
+    drone: true
+}
+
+function imprimirProfesiones(persona) {
+    console.log(`${persona.nombre} es:`)
+
+    if (persona.ingeniero) {
+        console.log('Ingeniero')
+    }
+}
