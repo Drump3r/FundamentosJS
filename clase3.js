2d1d1e2bdbde80c3a1dac37ba25cadbd526d58e3
Brian <jacklevybrsg@gmail.com>
2021-04-25 08:29:29 -0500

    Variables: Números

---
+++ clase3.js
@@ -0,0 +1,21 @@
+var edad = 27
+
+edad++
+
+var peso = 55
+
+peso -= 2
+
+var sandwich = 1
+
+peso = peso - sandwich
+
+var jugarFutbol = 3
+
+peso -= jugarFutbol
+
+var precioDeVino = 200.3
+
+var total = Math.round(precioDeVino *100 * 3)/100
+
+var totalStr = total.toFixed(3)
