80df323ffb063f87dad90b6a535461ab6ac71b08
Brian <jacklevybrsg@gmail.com>
2021-04-25 08:05:02 -0500

    Variables: Strings

---
+++ clase2.js
@@ -0,0 +1,11 @@
+var nombre = 'Andrea', apellido = 'Pancrasia';
+
+var nombreEnMayusculas = nombre.toLocaleUpperCase();
+var apellidoEnMinusculas = apellido.toLocaleLowerCase();
+
+var primeraLetraDelNombre = nombre.charAt(0);
+var cantidadDeLetrasDelNombre = nombre.length
+
+var nombreCompleto = `${nombre} ${apellido.toLocaleLowerCase()}`;
+
+var str = nombre.substr(1,2)
