2227332d1b62d04e0cbc6bb2ebd32803e7220ba7
Brian <jacklevybrsg@gmail.com>
2021-04-26 01:08:46 -0500

    Haciendo multiples requests

--- clase28.js
+++ clase28.js
@@ -1,9 +1,20 @@
 const API_URL = 'https://pokeapi.co/api/v2/'
 const POKEMON_URL = 'pokemon/:id'
-
-const bulbasaurUrl = `${API_URL}${POKEMON_URL.replace(':id', 1)}`
 const opts = {crossDomain: true}
 
-$.get(bulbasaurUrl, opts, function (pokemon) {
+const onPokemonResponse = function (pokemon) {
     console.log(`Has recivido un ${pokemon.name}`)
-})
+}
+function obtenerPokemon(id) {
+    const pokeUrl = `${API_URL}${POKEMON_URL.replace(':id', id)}`
+    //$.get(pokeUrl, opts, onPokemonResponse)
+    fetch(pokeUrl)
+    .then(function (res) {
+        return res.json()
+    })
+    .then(onPokemonResponse)
+    
+}
+obtenerPokemon(1)
+obtenerPokemon(2)
+obtenerPokemon(3)
