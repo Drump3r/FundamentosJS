8011553964aee03b55aa67c777c41f05e8349c74
Brian <jacklevybrsg@gmail.com>
2021-04-26 01:15:58 -0500

    Orden y el Asincronismo en JavaScript

--- clase28.js
+++ clase28.js
@@ -2,19 +2,25 @@ const API_URL = 'https://pokeapi.co/api/v2/'
 const POKEMON_URL = 'pokemon/:id'
 const opts = {crossDomain: true}
 
-const onPokemonResponse = function (pokemon) {
-    console.log(`Has recivido un ${pokemon.name}`)
-}
-function obtenerPokemon(id) {
+
+function obtenerPokemon(id, callback) {
     const pokeUrl = `${API_URL}${POKEMON_URL.replace(':id', id)}`
     //$.get(pokeUrl, opts, onPokemonResponse)
     fetch(pokeUrl)
     .then(function (res) {
         return res.json()
     })
-    .then(onPokemonResponse)
+    .then(function (pokemon) {
+        console.log(`Has recivido un ${pokemon.name}`)
+        if (callback) {
+            callback()
+        }
+    })
     
 }
-obtenerPokemon(1)
-obtenerPokemon(2)
-obtenerPokemon(3)
+obtenerPokemon(1, function () {
+    obtenerPokemon(2, function () {
+        obtenerPokemon(3)
+    })    
+})
+
