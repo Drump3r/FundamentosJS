1282c16785404d75c8cfb263d8c7b0afbda24e1c
Brian <jacklevybrsg@gmail.com>
2021-04-26 23:24:25 -0500

    promesas encadenadas

--- clase28.js
+++ clase28.js
@@ -1,26 +1,80 @@
 const API_URL = 'https://pokeapi.co/api/v2/'
 const POKEMON_URL = 'pokemon/:id'
-const opts = {crossDomain: true}
+//const opts = {crossDomain: true}
 
 
-function obtenerPokemon(id, callback) {
-    const pokeUrl = `${API_URL}${POKEMON_URL.replace(':id', id)}`
+function obtenerPokemon(id) {
+    return new Promise((resolve, reject) => {
+        const pokeUrl = `${API_URL}${POKEMON_URL.replace(':id', id)}`
+        fetch(pokeUrl)
+        .then((res) => res.json())
+        .then(function (pokemon) {
+            console.log(`Has recivido un ${pokemon.name}`)
+            resolve(pokemon)
+        }).catch(() => reject(id))
+    })
     //$.get(pokeUrl, opts, onPokemonResponse)
-    fetch(pokeUrl)
-    .then(function (res) {
-        return res.json()
-    })
-    .then(function (pokemon) {
-        console.log(`Has recivido un ${pokemon.name}`)
-        if (callback) {
-            callback()
-        }
-    })
+    
     
 }
-obtenerPokemon(1, function () {
-    obtenerPokemon(2, function () {
-        obtenerPokemon(3)
-    })    
-})
 
+
+
+obtenerPokemon(1)
+    .then(function (pokemon){
+        return obtenerPokemon(2)
+    })
+    .then(function (pokemon) {
+       return obtenerPokemon(3)
+    })
+    .then(function (pokemon) {
+        return obtenerPokemon(4)
+    })
+    .then(function (pokemon) {
+        return obtenerPokemon(5)
+    })
+    .then(function (pokemon) {
+        return obtenerPokemon(6)
+    })
+    .then(function (pokemon) {
+        return obtenerPokemon(7)
+    })
+    .then(function (pokemon) {
+        return obtenerPokemon(8)
+    })
+    .then(function (pokemon) {
+        return obtenerPokemon(9)
+    })
+    .then(function (pokemon) {
+        return obtenerPokemon(10)
+    })
+    .then(function (pokemon) {
+        return obtenerPokemon(11)
+    })
+    .then(function (pokemon) {
+        return obtenerPokemon(12)
+    })
+    .then(function (pokemon) {
+        return obtenerPokemon(13)
+    })
+    .then(function (pokemon) {
+        return obtenerPokemon(14)
+    })
+    .then(function (pokemon) {
+        return obtenerPokemon(15)
+    })
+    .then(function (pokemon) {
+        return obtenerPokemon(16)
+    })
+    .then(function (pokemon) {
+        return obtenerPokemon(17)
+    })
+    .then(function (pokemon) {
+        return obtenerPokemon(18)
+    })
+    .then(function (pokemon) {
+        return obtenerPokemon(19)
+    })
+    .then(function (pokemon) {
+        return obtenerPokemon(20)
+    })
